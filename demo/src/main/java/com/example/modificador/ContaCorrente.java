4529a2f6c3acf2d69ad49a959ddcdec7115c1463
Virna <virnanogueirati@gmail.com>
2025-03-19 15:22:30 -0300

    Add main method to ContaCorrente for testing Conta functionality

--- demo/src/main/java/com/example/modificador/ContaCorrente.java
+++ demo/src/main/java/com/example/modificador/ContaCorrente.java
@@ -1,7 +1,11 @@
 package com.example.modificador;
 
 public class ContaCorrente {
-
-    
+    public static void main(String[] args) {
+        Conta conta = new Conta(100.0);
+        conta.setTitular("João");
+        System.out.println(conta.getTitular());
+        System.out.println(conta.getSaldo());
+    }
 
 }
